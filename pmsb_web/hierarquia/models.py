a6ca174909db122dfe37c007d40a13e6c43e9af3
Jonatha-Varjao <jonathavarjao@uft.edu.br>
2018-05-03 21:10:01 -0300

    Importando os mixins do models questionarios
    TODO: Toda pessoa e usuaria?

--- pmsb_web/hierarquia/models.py
+++ pmsb_web/hierarquia/models.py
@@ -1,23 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+""" Model Mixins """
+from questionarios.models import UserOwnedModelMixin
+from questionarios.models import UUIDModelMixin
+""" Final Model Mixins """
 import uuid
 
 # Create your models here.
-""" Model Mixins """
-
-class UserOwnedModelMixin(models.Model):
-    owner = models.ForeignKey(User, on_delete = models.CASCADE)
-    class Meta:
-        abstract = True
-
-class UUIDModelMixin(models.Model):
-    id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
-    class Meta:
-        abstract = True
-
-""" Final Model Mixins """
-
-
 """ Hierarquia Horizontal  """
 
 class Departamento(UUIDModelMixin):
@@ -28,16 +18,27 @@ class Departamento(UUIDModelMixin):
         ordering = ["nome"]
         verbose_name = "Departamento";verbose_name_plural = "Departamentos"
     
-    def __str__(self):
-        return "{0}".format(self.nome)
+    def __concat_str__(self):
+        if self.departamento_pertencente is None:
+            return self.nome
+        else:
+            return self.departamento_pertencente.__concat_str__() + ' -> ' + self.nome
 
-class Pessoa(UUIDModelMixin, UserOwnedModelMixin):
+    def __str__(self):
+        return self.__concat_str__()
+
+class Pessoa(UUIDModelMixin):
     nome             = models.CharField(max_length = 255)
     isCoordenador    = models.BooleanField(null = False)
     departamento_pessoa = models.ForeignKey(Departamento, on_delete = models.CASCADE)
 
     class Meta:
-        ordering = ("isCoordenador", "nome")
+        ordering = ["-isCoordenador"]
         verbose_name = "Pessoa"
         verbose_name_plural = "Pessoas"
     
+    def __str__(self):
+        if self.isCoordenador:
+            return "Coordenador {0}".format(self.nome)
+        else:
+            return "{0}".format(self.nome)
