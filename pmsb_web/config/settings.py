1cfb4c64338ea38c82e5fb2ab738c461de50035e
Jonatha-Varjao <jonathavarjao@uft.edu.br>
2018-05-03 21:10:54 -0300

    adicionando app hierarquia no settings.py do projeto

--- pmsb_web/config/settings.py
+++ pmsb_web/config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'questionarios.apps.QuestionariosConfig',
-    'hierarquia',
+    'hierarquia.apps.HierarquiaConfig',
 ]
 
 MIDDLEWARE = [
