26844b7c78ad9a4270fd1349aac1e510d2bbff99
Paulo Roberto Cruz França <paulorobertocruz@users.noreply.github.com>
2018-05-10 15:47:04 -0300

    muda ordenação departamento

--- pmsb_web/hierarquia/models.py
+++ pmsb_web/hierarquia/models.py
@@ -12,18 +12,18 @@ import uuid
 
 class Departamento(UUIDModelMixin):
     nome = models.CharField(max_length = 255)
-    superior = models.ForeignKey('self', on_delete = models.CASCADE, blank=True,null = True)
+    superior = models.ForeignKey('self', on_delete = models.CASCADE, blank=True, null = True)
 
     class Meta:
-        ordering = ["nome"]
+        ordering = ["superior__nome", "nome"]
         verbose_name = "Departamento"
         verbose_name_plural = "Departamentos"
     
     def __concat_str__(self):
-        if self.departamento_pertencente is None:
+        if self.superior is None:
             return self.nome
         else:
-            return self.departamento_pertencente.__concat_str__() + ' -> ' + self.nome
+            return self.superior.__concat_str__() + ' -> ' + self.nome
 
     def __str__(self):
         return self.__concat_str__()
@@ -41,7 +41,7 @@ class Cargo(UUIDModelMixin):
 class Perfil(UUIDModelMixin):
     usuario = models.OneToOneField(User, on_delete = models.CASCADE)
     
-    superior = models.ForeignKey('self', on_delete = models.CASCADE, blank=True, null = True)
+    superior = models.ForeignKey('self', on_delete = models.CASCADE, blank=True, null = True, related_name="subordinados")
     departamento = models.ForeignKey(Departamento, on_delete = models.CASCADE)
     cargo = models.ForeignKey(Cargo, on_delete = models.CASCADE)
 
