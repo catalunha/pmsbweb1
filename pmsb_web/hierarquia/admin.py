c6e9a05435452d7bc3c336867d2ee3de9067f030
Jonatha Varjao <jonatha@baiano>
2018-06-08 02:37:49 -0300

    changing hierarquia app -> conta app

--- pmsb_web/hierarquia/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Departamento, Perfil
-# Register your models here.
-
-admin.site.register(Departamento)
-
-admin.site.register(Perfil)
--- pmsb_web/hierarquia/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class HierarquiaConfig(AppConfig):
-    name = 'hierarquia'
