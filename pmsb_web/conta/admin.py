c349c51499ac59b11d49aa9937519505b80393c2
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2018-06-13 20:13:45 -0300

    configura admin para conta

--- pmsb_web/conta/admin.py
+++ pmsb_web/conta/admin.py
@@ -1,5 +1,40 @@
 from django.contrib import admin
-from .models import Departamento, User
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Cargo, Departamento, Perfil, Atributo, ValorAtributo, DocumentoAtributo
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    pass
+
+#admin.site.register(User, CustomUserAdmin)
 admin.site.register(User)
-admin.site.register(Departamento)
+
+class DepartamentoAdmin(admin.ModelAdmin):
+    list_display = ("id", "nome", "descricao", "superior")
+
+admin.site.register(Departamento, DepartamentoAdmin)
+
+class CargoAdmin(admin.ModelAdmin):
+    list_display = ("id", "nome", "descricao")
+
+admin.site.register(Cargo, CargoAdmin)
+
+class PerfilAdmin(admin.ModelAdmin):
+    list_display = ("id", "usuario", "sexo")
+
+admin.site.register(Perfil, PerfilAdmin)
+
+class AtributoAdmin(admin.ModelAdmin):
+    list_display = ("id", "nome", "descricao", "valor", "documento")
+
+admin.site.register(Atributo, AtributoAdmin)
+
+class ValorAtributoAdmin(admin.ModelAdmin):
+    list_display = ("usuario", "tipo", "valor")
+
+admin.site.register(ValorAtributo, ValorAtributoAdmin)
+
+class DocumentoAtributoAdmin(admin.ModelAdmin):
+    list_display = ("id", "usuario", "tipo")
+
+admin.site.register(DocumentoAtributo, DocumentoAtributoAdmin)
