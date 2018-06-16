89445a6c7a7cd1a7fa80f1485b487c108f1b4ff9
Varjao <jonathavarjao@uft.edu.br>
2018-06-16 02:00:07 -0300

    adequando o crude pelo admin para o usuario/perfil

--- pmsb_web/conta/admin.py
+++ pmsb_web/conta/admin.py
@@ -1,13 +1,27 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django import forms
+
+# django project imports
+from .forms import RegisterUserForm, AtualizarPerfilForm
 from .models import User, Cargo, Departamento, Perfil, Atributo, ValorAtributo, DocumentoAtributo
 # Register your models here.
 
-class CustomUserAdmin(UserAdmin):
-    pass
+ADDITIONAL_USER_FIELDS = (
+    (None, {'fields': ('cpf','email','foto','first_name','last_name','departamento','superior','cargo','data_nascimento')}),
+)
 
-#admin.site.register(User, CustomUserAdmin)
-admin.site.register(User)
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+
+    add_fieldsets = UserAdmin.add_fieldsets + ADDITIONAL_USER_FIELDS
+    fieldsets = UserAdmin.fieldsets + ADDITIONAL_USER_FIELDS
+
+    list_display = ('id', 'username', 'first_name', 'last_name', 'cpf', 'email', 'departamento', 'cargo', 'data_nascimento')
+
+admin.site.register(User, CustomUserAdmin)
 
 class DepartamentoAdmin(admin.ModelAdmin):
     list_display = ("id", "nome", "descricao", "superior")
