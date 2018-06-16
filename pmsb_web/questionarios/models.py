43b962012a9f2cf14b8b6d3ffbc5b5f149faf197
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2018-06-16 02:18:27 -0300

    add managers explicitos, remove metodos inutilizados em RespostaPergunta, add atributo booleano multipla em PerguntaUnicaEscolha

--- pmsb_web/questionarios/models.py
+++ pmsb_web/questionarios/models.py
@@ -9,6 +9,8 @@ class Localizacao(models.Model):
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
     altitude = models.DecimalField(max_digits=9, decimal_places=6)
 
+    objects = models.Manager()
+
     def __str__(self):
         return "latitude:{} longitute:{} altitude:{}".format(self.latitude, self.longitude, self.altitude)
 
@@ -27,6 +29,8 @@ class Questionario(UUIDModelMixin, UserOwnedModelMixin, TimedModelMixin):
         through_fields = ("questionario","pergunta"),
         related_name = "questionarios",
     )
+
+    objects = models.Manager()
 
     class Meta:
         verbose_name = "Questionario"
@@ -48,6 +52,8 @@ class Pergunta(UUIDModelMixin, TimedModelMixin):
     
     possivel_escolha_requisito = models.ForeignKey("PossivelEscolha", on_delete = models.SET_NULL, null = True, blank = True, related_name="pre_requisito_de")
 
+    objects = models.Manager()
+
     class Meta:
         ordering = ("tipo",)
         verbose_name = "Pergunta"
@@ -67,6 +73,8 @@ class UnidadeMedida(models.Model):
     nome = models.CharField(max_length = 255, unique = True)
     sigla = models.CharField(max_length = 5, unique = True)
 
+    objects = models.Manager()
+
     class Meta:
         verbose_name = "Unidade de Medida"
         verbose_name_plural = "Unidades de Medida"
@@ -78,6 +86,7 @@ class UnidadeMedida(models.Model):
 
 class PerguntaUnicaEscolha(Pergunta):
     TIPO = 0
+    multipla = models.BooleanField(defalt = False)
 
 class PerguntaMultiplaEscolha(Pergunta):
     TIPO = 1
@@ -106,6 +115,8 @@ class PerguntaDoQuestionario(UUIDModelMixin, TimedModelMixin):
     pergunta = models.ForeignKey(Pergunta, on_delete = models.CASCADE)
     ordem = models.PositiveSmallIntegerField()
 
+    objects = models.Manager()
+
     class Meta:
         unique_together = ("questionario", "pergunta")
         ordering = ("questionario","ordem","pergunta")
@@ -114,6 +125,8 @@ class PerguntaDoQuestionario(UUIDModelMixin, TimedModelMixin):
 class PossivelEscolha(UUIDModelMixin, TimedModelMixin):
     pergunta = models.ForeignKey(Pergunta, on_delete = models.CASCADE, related_name="possiveis_escolhas")
     texto = models.TextField()
+
+    objects = models.Manager()
 
     class Meta:
         verbose_name = "Possivel Escolha"
@@ -138,26 +151,18 @@ class RespostaPergunta(UUIDModelMixin, TimedModelMixin):
     pergunta = models.ForeignKey(Pergunta, on_delete = models.CASCADE)
     localizacao = models.ForeignKey(Localizacao, on_delete = models.CASCADE, null = True, blank = True)
 
+    objects = models.Manager()
+
     class Meta:
         unique_together = ("resposta_questionario", "pergunta")
         verbose_name = "Resposta Pergunta"
         verbose_name_plural = "Respostas Perguntas"
 
-    @property
-    def tipo(self):
-        return self.pergunta.tipo
-
-    def set_resposta(self, instance):
-        if self.tipo == "":
-            pass
-        
-        if isinstance(instance, PossivelEscolhaResposta):
-            pass
-
-
 class PossivelEscolhaResposta(UUIDModelMixin, TimedModelMixin):
     resposta_pergunta = models.ForeignKey(RespostaPergunta, on_delete = models.CASCADE, related_name="escolhas")
     possivel_escolha = models.ForeignKey(PossivelEscolha, on_delete = models.CASCADE)
+
+    objects = models.Manager()
 
     class Meta:
         unique_together = ("resposta_pergunta", "possivel_escolha")
@@ -166,14 +171,20 @@ class CoordenadaResposta(UUIDModelMixin, TimedModelMixin):
     resposta_pergunta = models.ForeignKey(RespostaPergunta, on_delete = models.CASCADE, related_name="coordenadas")
     coordenada = models.ForeignKey(Localizacao, on_delete = models.CASCADE, null = True)
 
+    objects = models.Manager()
+
 class TextoResposta(UUIDModelMixin, TimedModelMixin):
     resposta_pergunta = models.ForeignKey(RespostaPergunta, on_delete = models.CASCADE, related_name="textos")
     texto = models.TextField()
+
+    objects = models.Manager()
 
 class NumeroResposta(UUIDModelMixin, TimedModelMixin):
     resposta_pergunta = models.ForeignKey(RespostaPergunta, on_delete = models.CASCADE, related_name="numeros")
     unidade_medida = models.ForeignKey(UnidadeMedida, on_delete = models.CASCADE)
     numero = models.FloatField()
+
+    objects = models.Manager()
 
 def caminho_para_arquivos(instance, filename):
     hoje = timezone.now()
@@ -189,6 +200,8 @@ class ArquivoResposta(UUIDModelMixin, TimedModelMixin):
     resposta_pergunta = models.ForeignKey(RespostaPergunta, on_delete = models.CASCADE, related_name="arquivos")
     arquivo = models.FileField(upload_to=caminho_para_arquivos)
 
+    objects = models.Manager()
+
 def caminho_para_imagens(instance, filename):
     hoje = timezone.now()
     try:
@@ -202,3 +215,5 @@ def caminho_para_imagens(instance, filename):
 class ImagemResposta(UUIDModelMixin, TimedModelMixin):
     resposta_pergunta = models.ForeignKey(RespostaPergunta, on_delete = models.CASCADE, related_name="imagens")
     imagem = models.ImageField(upload_to=caminho_para_imagens)
+
+    objects = models.Manager()
