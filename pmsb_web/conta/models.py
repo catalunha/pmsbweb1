f10be9d1ab3321f36f85849f145ec9a2aed4edcb
Varjao <jonathavarjao@uft.edu.br>
2018-06-16 01:59:42 -0300

    blank,null = true em alguns campos
    a pedido do catalunha

--- pmsb_web/conta/models.py
+++ pmsb_web/conta/models.py
@@ -1,9 +1,10 @@
+# encoding: utf-8
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 """ Model Mixins """
-from questionarios.models import UserOwnedModelMixin
-from questionarios.models import UUIDModelMixin
+from core.mixins import UserOwnedModelMixin
+from core.mixins import UUIDModelMixin
 """ Final Model Mixins """
 
 """ Hierarquia Horizontal  """
@@ -42,7 +43,7 @@ class Cargo(UUIDModelMixin):
 def upload_foto_usuario(instance, filename):
     return "{0}_{1}".format(instance.pk, filename)
 
-class User(AbstractUser):
+class User(AbstractUser, UUIDModelMixin):
 
     cpf = models.CharField(max_length=11, unique = True, verbose_name="CPF")
 
@@ -53,19 +54,23 @@ class User(AbstractUser):
     superior = models.ForeignKey('self', on_delete = models.SET_NULL, blank = True, null = True, related_name="subordinados")
 
     # referencias com as tabelas
-    departamento = models.ForeignKey(Departamento, on_delete = models.SET_NULL, null = True)
+    departamento = models.ForeignKey(Departamento, on_delete = models.SET_NULL, blank=True,null = True)
 
     # cargo
-    cargo = models.ForeignKey(Cargo, on_delete = models.SET_NULL, null = True)
+    cargo = models.ForeignKey(Cargo, on_delete = models.SET_NULL, blank=True,null = True)
 
     #data nascimento
-    data_nascimeto = models.DateField(blank=True, null = True, verbose_name="Data de Nascimento")
+    data_nascimento = models.DateField(blank=True, null = True, verbose_name="Data de Nascimento")
+
+    def __str__(self):
+        return "{0}: {1} {2}".format(self.departamento, self.first_name, self.last_name)
 
 class Perfil(UUIDModelMixin):
 
     SEXO_CHOICES = (
-        ("M", "Masculino"),
-        ("F", "Feminino"),
+        (u'M', u'Masculino'),
+        (u'F', u'Feminino'),
+        (u'ND', u'Nao-Declara')
     )
 
     ESTADOS_CHOICES = (
