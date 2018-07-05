4067a6214beab3958eb641e75a5a0774d6f815d3
Varjao <jonathavarjao@uft.edu.br>
2018-07-05 10:58:34 -0300

    adequando path pra salvar os documentos do perfil

--- pmsb_web/conta/models.py
+++ pmsb_web/conta/models.py
@@ -85,7 +85,7 @@ class ValorAtributo(UUIDModelMixin, UserOwnedModelMixin, TimedModelMixin):
         return "{}-{}".format(self.tipo, self.valor)
 
 def documento_atributo(instance, filename):
-    return "documentos_atributo/{}/{}/{}".format(instance.usuario_id, instance.tipo.nome, instance.usuario_id+filename)
+    return "documentos_atributo/{0}/{1}/{0}_{2}".format(instance.usuario_id, instance.tipo.nome, filename)
 
 class DocumentoAtributo(UUIDModelMixin, UserOwnedModelMixin, TimedModelMixin):
     """
