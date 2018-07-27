a5d2eb2a0460d38605bf9dacab6b068a5b605878
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2018-07-26 22:39:44 -0300

    esboça lista de views que serão utilizadas para criação dos formularios

--- pmsb_web/questionarios/views.py
+++ pmsb_web/questionarios/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic import CreateView, UpdateView, ListView, TemplateView
-# Create your views here.
-from .models import Questionario
+
+from .models import Questionario, Pergunta, PerguntaDoQuestionario
 
 class ListarQuestionarios(ListView):
     model = Questionario
+    template_name = "questionarios/listar.html"
 
 class CriarQuesqionario(CreateView):
-    pass
+    template_name = "questionarios/criar.html"
+    success_url = reverse_lazy("questionarios:editar")
 
 class EditarQuestioanrio(UpdateView):
-    pass
+    template_name = "questionarios/editar.html"
+
+class AdicionaPerguntaView(CreateView):
+    model = Pergunta
+    template_name = "questionarios/adicionar_pergunta.html"
 
 class TesteView(TemplateView):
     template_name = "questionarios/listar_questionarios.html"
