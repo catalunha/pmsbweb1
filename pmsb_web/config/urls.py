1aa49a5d9132f8a219a942fd287a71227553cea1
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2018-07-29 21:28:54 -0300

    adiciona media nas urls somente em debug = true

--- pmsb_web/config/urls.py
+++ pmsb_web/config/urls.py
@@ -24,4 +24,7 @@ urlpatterns = [
     path('api/', include("questionarios.api_urls")),
     path('tarefas/', include('pinax.messages.urls', namespace='pinax_messages')),
     path("questionarios/", include("questionarios.urls")),  
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
