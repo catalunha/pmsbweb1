291576be22feb18991392338368a67ef0d313956
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2018-07-31 19:37:42 -0300

    adiciona forms para todos os tipos de perguntas

--- pmsb_web/questionarios/forms.py
+++ pmsb_web/questionarios/forms.py
@@ -1,7 +1,47 @@
 from django import forms
-from .models import Questionario
+
+from .models import (
+    Questionario,
+    Pergunta,
+    PerguntaArquivo,
+    PerguntaCoordenada,
+    PerguntaEscolha,
+    PerguntaImagem,
+    PerguntaNumero,
+    PerguntaTexto,
+)
 
 class QuestionarioForm(forms.ModelForm):
     class Meta:
         model = Questionario
         fields = ("nome", )
+
+
+class BasePerguntaForm(forms.ModelForm):
+    class Meta:
+        model = Pergunta
+        fields = "__all__"
+
+class PerguntaArquivoForm(BasePerguntaForm):
+    class Meta(BasePerguntaForm.Meta):
+        model = PerguntaArquivo
+
+class PerguntaCoordenadaForm(BasePerguntaForm):
+    class Meta(BasePerguntaForm.Meta):
+        model = PerguntaCoordenada
+
+class PerguntaEscolhaForm(BasePerguntaForm):
+    class Meta(BasePerguntaForm.Meta):
+        model = PerguntaEscolha
+
+class PerguntaImagemForm(BasePerguntaForm):
+    class Meta(BasePerguntaForm.Meta):
+        model = PerguntaImagem
+
+class PerguntaNumeroForm(BasePerguntaForm):
+    class Meta(BasePerguntaForm.Meta):
+        model = PerguntaNumero
+
+class PerguntaTextoForm(BasePerguntaForm):
+    class Meta(BasePerguntaForm.Meta):
+        model = PerguntaTexto
