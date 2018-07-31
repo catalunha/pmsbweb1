23175b6a6a025b2f47e60f9e0c4c2c572fd4d4f0
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2018-07-31 19:38:25 -0300

    cria views de crud de questionario e perguntas

--- pmsb_web/questionarios/urls.py
+++ pmsb_web/questionarios/urls.py
@@ -6,5 +6,17 @@ app_name = "questionarios"
 from . import views
 
 urlpatterns = [
-    path("teste/", views.TesteView.as_view(), name = "teste"),  
+
+    # questionarios
+    path("", views.QuestionarioListView.as_view(), name = "list"),
+    path("criar/", views.QuestionarioCreateView.as_view(), name = "create"),
+    path("editar/<int:pk>/", views.QuestioanrioUpdateView.as_view(), name = "update"),
+    path("ordenar/<int:pk>",views.QuestionarioOrdenarView.as_view(), name = "ordenar"),
+    path("delete/<int:pk>/", views.QuestioanrioDeleteView.as_view(), name = "delete"),
+
+    #perguntas
+    path("criar/pergunta/tipo/", views.PerguntaEscolherTipoTemplateView.as_view(), name = "create_pergunta_tipo"),
+    path("criar/pergunta/<int:pk>/tipo/<int:tipo>/", views.PerguntaCreateView.as_view(), name = "create_pergunta"),
+    path("editar/pergunta/<int:pk>/", views.PerguntaUpdateView.as_view(), name = "update_pergunta"),
+    path("delete/pergunta/<int:pk>/", views.PerguntaDeleteView.as_view(), name = "delete_pergunta"),
 ]
--- pmsb_web/questionarios/views.py
+++ pmsb_web/questionarios/views.py
@@ -1,24 +1,83 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic import CreateView, UpdateView, ListView, TemplateView
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView, TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
-from .models import Questionario, Pergunta, PerguntaDoQuestionario
+from .models import (
+    Questionario,
+    Pergunta,
+    PerguntaArquivo,
+    PerguntaCoordenada,
+    PerguntaEscolha,
+    PerguntaImagem,
+    PerguntaNumero,
+    PerguntaTexto,
+    PerguntaDoQuestionario,
+)
+from .forms import (
+    QuestionarioForm,
+    PerguntaArquivoForm,
+    PerguntaCoordenadaForm,
+    PerguntaEscolhaForm,
+    PerguntaImagemForm,
+    PerguntaNumeroForm,
+    PerguntaTextoForm,
+)
 
-class ListarQuestionarios(ListView):
+class QuestionarioListView(LoginRequiredMixin, ListView):
     model = Questionario
-    template_name = "questionarios/listar.html"
+    template_name = "questionarios/list_questionario.html"
 
-class CriarQuesqionario(CreateView):
-    template_name = "questionarios/criar.html"
-    success_url = reverse_lazy("questionarios:editar")
+class QuestionarioCreateView(LoginRequiredMixin, CreateView):
+    model = Questionario
+    form_class = QuestionarioForm
+    template_name = "questionarios/create_questionario.html"
+    success_url = reverse_lazy("questionarios:list")
 
-class EditarQuestioanrio(UpdateView):
-    template_name = "questionarios/editar.html"
+class QuestioanrioUpdateView(LoginRequiredMixin, UpdateView):
+    template_name = "questionarios/update_questionario.html"
+    model = Questionario
+    form_class = QuestionarioForm
+    success_url = reverse_lazy("questionarios:list")
 
-class AdicionaPerguntaView(CreateView):
+class QuestionarioOrdenarView(View):
+    pass 
+
+class QuestioanrioDeleteView(DeleteView):
+    template_name = "questionarios/delete_questionario.html"
+    model = Questionario
+    success_url = reverse_lazy("questionarios:list")
+
+class PerguntaEscolherTipoTemplateView(LoginRequiredMixin, TemplateView):
+    template_name = "questionarios/escolher_tipo_pergunta.html"
+
+class PerguntaCreateView(LoginRequiredMixin, CreateView):
     model = Pergunta
-    template_name = "questionarios/adicionar_pergunta.html"
+    template_name = "questionarios/create_pergunta.html"
 
-class TesteView(TemplateView):
-    template_name = "questionarios/listar_questionarios.html"
+class PerguntaUpdateView(LoginRequiredMixin, UpdateView):
+    model = Pergunta
+    template_name = "questionarios/update_pergunta.html"
+
+    def get_form_class(self):
+        
+        instance = self.object.cast()
+
+        if isinstance(instance, PerguntaEscolha):
+            return PerguntaEscolhaForm
+        elif isinstance(instance, PerguntaNumero):
+            return PerguntaNumeroForm
+        elif isinstance(instance, PerguntaArquivo):
+            return PerguntaArquivoForm
+        elif isinstance(instance, PerguntaTexto):
+            return PerguntaTextoForm
+        elif isinstance(instance, PerguntaCoordenada):
+            return PerguntaCoordenadaForm
+        elif isinstance(instance, PerguntaImagem):
+            return PerguntaImagemForm
+        else:
+            return None
+
+class PerguntaDeleteView(DeleteView):
+    pass
