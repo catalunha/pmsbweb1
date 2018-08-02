390777e0138a54e2dfb0ec7808724276de6377fb
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2018-08-01 21:48:14 -0300

    QuestionarioListView lista somente questionarios do usuario, QuestionarioCreateView questionario é criado com usuario logado como criador, QuestionarioDeleteView espera confirmação do id por um formulario para deletar a instancia, PerguntaCreateView e PerguntaUpdateView pegam a instancia de formulario correta dinamicamente, PerguntaDoQuestionarioDeleteView deleta domente ligação do questionario com pergunta

--- pmsb_web/questionarios/views.py
+++ pmsb_web/questionarios/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic import CreateView, UpdateView, DeleteView, ListView, TemplateView
+from django.views.generic import (
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+    ListView,
+    TemplateView,
+    FormView,
+)
+from django.views.generic.edit import FormMixin, ProcessFormView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from .models import (
@@ -17,6 +26,8 @@ from .models import (
 )
 from .forms import (
     QuestionarioForm,
+    BasePerguntaForm,
+    QuestionarioDeleteForm,
     PerguntaArquivoForm,
     PerguntaCoordenadaForm,
     PerguntaEscolhaForm,
@@ -25,15 +36,24 @@ from .forms import (
     PerguntaTextoForm,
 )
 
+
 class QuestionarioListView(LoginRequiredMixin, ListView):
     model = Questionario
     template_name = "questionarios/list_questionario.html"
+
+    def get_queryset(self):
+        queryset = super(QuestionarioListView, self).get_queryset()
+        return queryset.filter(usuario = self.request.user)
 
 class QuestionarioCreateView(LoginRequiredMixin, CreateView):
     model = Questionario
     form_class = QuestionarioForm
     template_name = "questionarios/create_questionario.html"
     success_url = reverse_lazy("questionarios:list")
+
+    def form_valid(self, form):
+        form.instance.usuario = self.request.user
+        return super(QuestionarioCreateView, self).form_valid(form)
 
 class QuestioanrioUpdateView(LoginRequiredMixin, UpdateView):
     template_name = "questionarios/update_questionario.html"
@@ -44,27 +64,74 @@ class QuestioanrioUpdateView(LoginRequiredMixin, UpdateView):
 class QuestionarioOrdenarView(View):
     pass 
 
-class QuestioanrioDeleteView(DeleteView):
+class QuestioanrioDeleteView(FormMixin, DeleteView):
     template_name = "questionarios/delete_questionario.html"
     model = Questionario
+    form_class = QuestionarioDeleteForm
     success_url = reverse_lazy("questionarios:list")
 
-class PerguntaEscolherTipoTemplateView(LoginRequiredMixin, TemplateView):
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        form.object_id = self.kwargs.get("pk")
+        if form.is_valid():
+            return super(QuestioanrioDeleteView, self).post(request, *args, **kwargs)
+        else:
+            return self.form_invalid(form)
+
+
+
+    
+class PerguntaEscolherTipoTemplateView(LoginRequiredMixin, DetailView):
+    model = Questionario
     template_name = "questionarios/escolher_tipo_pergunta.html"
 
 class PerguntaCreateView(LoginRequiredMixin, CreateView):
-    model = Pergunta
     template_name = "questionarios/create_pergunta.html"
-
-class PerguntaUpdateView(LoginRequiredMixin, UpdateView):
     model = Pergunta
-    template_name = "questionarios/update_pergunta.html"
-
-    def get_object(self):
-        self.object = self.object.cast()
+    form_class = BasePerguntaForm
+    success_url = reverse_lazy("questionarios:list")
 
     def get_form_class(self):        
-        instance = self.object
+        instance = self.kwargs.get("tipo")
+        if instance == 0:
+            return PerguntaEscolhaForm
+        elif instance == 1:
+            return PerguntaTextoForm
+        elif instance == 2:
+            return PerguntaArquivoForm
+        elif instance == 3:
+            return PerguntaImagemForm
+        elif instance == 4:
+            return PerguntaCoordenadaForm
+        elif instance == 5:
+            return PerguntaNumeroForm
+        else:
+            return None
+
+    def form_valid(self, form):
+        form_valid_return = super(PerguntaCreateView, self).form_valid(form)
+        
+        questionario = get_object_or_404(Questionario, pk = self.kwargs.get("pk"))
+        pergunta_questionario = PerguntaDoQuestionario()
+        pergunta_questionario.questionario = questionario
+        pergunta_questionario.pergunta = self.object
+        pergunta_questionario.save()
+
+        return form_valid_return
+
+class PerguntaUpdateView(LoginRequiredMixin, UpdateView):
+    template_name = "questionarios/update_pergunta.html"
+    model = Pergunta
+    form_class = BasePerguntaForm
+
+    def get_object(self):
+        self.object = super(PerguntaUpdateView, self).get_object()
+        self.object = self.object.cast()
+        return self.object
+
+    def get_form_class(self):        
+        instance = self.get_object()
 
         if isinstance(instance, PerguntaEscolha):
             return PerguntaEscolhaForm
@@ -81,5 +148,8 @@ class PerguntaUpdateView(LoginRequiredMixin, UpdateView):
         else:
             return None
 
-class PerguntaDeleteView(DeleteView):
-    pass
+class PerguntaDoQuestionarioDeleteView(LoginRequiredMixin, DeleteView):
+    template_name = "questionarios/delete_pergunta_do_questionario.html"
+    model = PerguntaDoQuestionario
+    success_url = reverse_lazy("questionarios:list")
+
