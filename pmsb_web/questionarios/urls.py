15d47823905c67890ff3064620c893bc5eb36d17
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2018-08-01 21:37:33 -0300

    caminhos para views de crud de questionario com perguntas

--- pmsb_web/questionarios/urls.py
+++ pmsb_web/questionarios/urls.py
@@ -10,13 +10,13 @@ urlpatterns = [
     # questionarios
     path("", views.QuestionarioListView.as_view(), name = "list"),
     path("criar/", views.QuestionarioCreateView.as_view(), name = "create"),
-    path("editar/<int:pk>/", views.QuestioanrioUpdateView.as_view(), name = "update"),
-    path("ordenar/<int:pk>",views.QuestionarioOrdenarView.as_view(), name = "ordenar"),
-    path("delete/<int:pk>/", views.QuestioanrioDeleteView.as_view(), name = "delete"),
+    path("editar/<uuid:pk>/", views.QuestioanrioUpdateView.as_view(), name = "update"),
+    path("ordenar/<uuid:pk>",views.QuestionarioOrdenarView.as_view(), name = "ordenar"),
+    path("delete/<uuid:pk>/", views.QuestioanrioDeleteView.as_view(), name = "delete"),
 
     #perguntas
-    path("criar/pergunta/tipo/", views.PerguntaEscolherTipoTemplateView.as_view(), name = "create_pergunta_tipo"),
-    path("criar/pergunta/<int:pk>/tipo/<int:tipo>/", views.PerguntaCreateView.as_view(), name = "create_pergunta"),
-    path("editar/pergunta/<int:pk>/", views.PerguntaUpdateView.as_view(), name = "update_pergunta"),
-    path("delete/pergunta/<int:pk>/", views.PerguntaDeleteView.as_view(), name = "delete_pergunta"),
+    path("criar/pergunta/tipo/<uuid:pk>/", views.PerguntaEscolherTipoTemplateView.as_view(), name = "create_pergunta_tipo"),
+    path("criar/pergunta/<uuid:pk>/tipo/<int:tipo>/", views.PerguntaCreateView.as_view(), name = "create_pergunta"),
+    path("editar/<uuid:questionaio_pk>/pergunta/<uuid:pk>/", views.PerguntaUpdateView.as_view(), name = "update_pergunta"),
+    path("delete/pergunta/<uuid:pk>/", views.PerguntaDoQuestionarioDeleteView.as_view(), name = "delete_pergunta_do_questionario"),
 ]
