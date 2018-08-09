f7ab789f318f2a0488dc9dbb777bacbe166e8ff5
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2018-08-09 19:35:49 -0300

    add link para update_possivelescolha e muda link para update_pergunta

--- pmsb_web/questionarios/urls.py
+++ pmsb_web/questionarios/urls.py
@@ -17,11 +17,12 @@ urlpatterns = [
     #perguntas
     path("criar/pergunta/tipo/<uuid:pk>/", views.PerguntaEscolherTipoTemplateView.as_view(), name = "create_pergunta_tipo"),
     path("criar/pergunta/<uuid:pk>/tipo/<int:tipo>/", views.PerguntaCreateView.as_view(), name = "create_pergunta"),
-    path("editar/pergunta/<uuid:pk>/", views.PerguntaUpdateView.as_view(), name = "update_pergunta"),
+    path("editar/<uuid:questionario_pk>/pergunta/<uuid:pk>/", views.PerguntaUpdateView.as_view(), name = "update_pergunta"),
     path("delete/pergunta/<uuid:pk>/", views.PerguntaDoQuestionarioDeleteView.as_view(), name = "delete_pergunta_do_questionario"),
 
     #possivel escolha
     path("editar/pergunta/<uuid:pk>/criar_escolha", views.PossivelEscolhaCreateView.as_view(), name = "create_possivelescolha"),
+    path("editar/pergunta/<uuid:pergunta_pk>/editar_escolha/<uuid:pk>", views.PossivelEscolhaUpdateView.as_view(), name = "update_possivelescolha"),
     path("editar/pergunta/<uuid:pergunta_pk>/delete_escolha/<uuid:pk>", views.PossivelEscolhaDeleteView.as_view(), name = "delete_possivelescolha"),
 
     path("teste/", views.TesteTemplateView.as_view(), name = "teste"),
