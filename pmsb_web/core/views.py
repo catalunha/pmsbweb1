4b82886b45168ac0679ec2f9ac56045d58271ae3
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2018-08-25 14:26:53 -0300

    FakeDeleteView para ser utilizada em modelos com FakeDeleteModelMixin

--- pmsb_web/core/views.py
+++ pmsb_web/core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import DeleteView
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+class FakeDeleteView(DeleteView):
+    def delete(self, request, *args, **kwargs):
+        obj = self.get_object()
+        fk_metodo = getattr(obj, "fake_delete", None)
+
+        if callable(fk_metodo):
+            obj.fake_delete()
+        else:
+            raise Exception("model não tem metodo fake_delete")
+        
+        return HttpResponseRedirect(self.get_success_url())
