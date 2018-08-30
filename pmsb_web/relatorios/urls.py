39807269b40e8a56768a0503f24f1417703a94ec
Varjao <jonathavarjao@uft.edu.br>
2018-08-30 19:35:18 -0300

    adicionando url de editar bloco e texto

--- pmsb_web/relatorios/urls.py
+++ pmsb_web/relatorios/urls.py
@@ -16,6 +16,8 @@ urlpatterns = [
     #blocos
     path("<uuid:pk>/bloco", views.BlocoCreateView.as_view(), name="create_bloco"),
     path("<uuid:pk>/bloco/<uuid:nivel_superior_pk>/", views.BlocoCreateView.as_view(), name="create_bloco_nivel_superior"),
+    path("bloco/<uuid:pk>/editar/", views.BlocoUpdateView.as_view(), name="update_bloco"),
+    path("bloco/<uuid:pk>/texto", views.BlocoTextoCreateView.as_view(), name="update_text_bloco"),
 
     #figuras
     path("<uuid:relatorio_pk>/figuras/", views.FiguraListView.as_view(), name = "list_figura"),
