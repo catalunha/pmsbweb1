f01acd7f85d01f34c3f6d1aa598dc99491d2f195
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2018-08-30 21:28:01 -0300

    salva self.objeto com resultado de get_object() que estava causando erros em outros metodos

--- pmsb_web/core/views.py
+++ pmsb_web/core/views.py
@@ -4,11 +4,11 @@ from django.http import HttpResponseRedirect
 
 class FakeDeleteView(DeleteView):
     def delete(self, request, *args, **kwargs):
-        obj = self.get_object()
-        fk_metodo = getattr(obj, "fake_delete", None)
+        self.object = self.get_object()
+        fk_metodo = getattr(self.object, "fake_delete", None)
 
         if callable(fk_metodo):
-            obj.fake_delete()
+            self.object.fake_delete()
         else:
             raise Exception("model não tem metodo fake_delete")
         
