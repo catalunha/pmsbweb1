0f3bd27b767569b3f634408d34212611c37fbf13
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2018-11-06 20:17:03 -0300

    add forms para PerguntaRequisito e EscolhaRequisito

--- pmsb_web/questionarios/forms.py
+++ pmsb_web/questionarios/forms.py
@@ -9,7 +9,10 @@ from .models import (
     PerguntaImagem,
     PerguntaNumero,
     PerguntaTexto,
+    PerguntaDoQuestionario,
     PossivelEscolha,
+    PerguntaRequisito,
+    EscolhaRequisito,
 )
 
 class QuestionarioForm(forms.ModelForm):
@@ -29,7 +32,7 @@ class QuestionarioDeleteForm(forms.Form):
 class BasePerguntaForm(forms.ModelForm):
     class Meta:
         model = Pergunta
-        fields = ("variavel", "texto", "possivel_escolha_requisito")
+        fields = ("variavel", "texto", )
 
 class PerguntaArquivoForm(BasePerguntaForm):
     class Meta(BasePerguntaForm.Meta):
@@ -73,3 +76,19 @@ class PerguntaRequisitoHiddenChangeForm(forms.ModelForm):
         widgets = {
             "pergunta_requisito": forms.HiddenInput(),
         }
+
+class CreatePerguntaRequisitoForm(forms.ModelForm):
+    questionario = forms.ModelChoiceField(queryset = Questionario.objects.all())
+    pergunta_requisito = forms.ModelChoiceField(queryset = PerguntaDoQuestionario.objects.all())
+    class Meta:
+        model = PerguntaRequisito
+        fields = ("questionario","pergunta_requisito")
+
+class CreateEscolhaRequisitoForm(forms.ModelForm):
+    questionario = forms.ModelChoiceField(queryset = Questionario.objects.all())
+    pergunta_requisito = forms.ModelChoiceField(queryset = PerguntaDoQuestionario.objects.all()) 
+    escolha_requisito = forms.ModelChoiceField(queryset = PossivelEscolha.objects.all())
+
+    class Meta:
+        model = EscolhaRequisito
+        fields = ("questionario", "pergunta_requisito", "escolha_requisito")
