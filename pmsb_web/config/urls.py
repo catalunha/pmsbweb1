4a1e90b7f4c86896f82d3940ab7432aa48f5d904
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2019-03-20 22:16:00 -0300

    se debug ativado habilita documentação online

--- pmsb_web/config/urls.py
+++ pmsb_web/config/urls.py
@@ -1,10 +1,11 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
+from rest_framework_swagger.views import get_swagger_view
 
 urlpatterns = [
-    path('pmsb/', include(('conta.urls','conta'), namespace='conta')),
+    path('pmsb/', include(('conta.urls', 'conta'), namespace='conta')),
     path('pmsb/admin/', admin.site.urls),
     path('pmsb/api/', include("questionarios.api_urls")),
     path('pmsb/tarefas/', include('pinax.messages.urls', namespace='pinax_messages')),
@@ -14,3 +15,6 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [
+        path('pmsb/docs/api/swagger', get_swagger_view(title="PMSB API")),
+    ]
