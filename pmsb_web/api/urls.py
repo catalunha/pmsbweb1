ae3daca4ef76c4ef9e54e587a4e6ad08743facd9
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2019-04-03 15:31:23 -0300

    api view respondendo a versão mais recente do app

--- pmsb_web/api/urls.py
+++ pmsb_web/api/urls.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_swagger.views import get_swagger_view
+from api.views import VersaoAPIView
 
 app_name = 'api'
 
 urlpatterns = [
+    path('versao/', VersaoAPIView.as_view(), name='versao'),
     path('auth/', include('api.urls_auth')),
     path('questionarios/', include('questionarios.api_urls')),
     path('docs/', include('api.urls_docs')),
--- pmsb_web/api/views.py
+++ pmsb_web/api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
-# Create your views here.
+from api.models import MobileApp
+
+class VersaoAPIView(APIView):
+    permission_classes = (AllowAny, )
+
+    def get(self, request, formt=None):
+        """
+        Retorna versão atual do app mobile
+        """
+        latest = MobileApp.latest()
+
+        return Response(latest)
