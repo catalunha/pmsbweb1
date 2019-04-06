c726265024b80b8b0761a42e1d0f14d54d08bd39
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2019-04-06 15:12:18 -0300

    add campo boleano ativo para poder marcar setorCensitario que deve ser ignorado

--- pmsb_web/questionarios/models.py
+++ pmsb_web/questionarios/models.py
@@ -361,6 +361,7 @@ class SetorCensitario(UUIDModelMixin, FakeDeleteModelMixin, TimedModelMixin):
     nome = models.CharField(max_length=255, unique=True)
     setor_superior = models.ForeignKey("SetorCensitario", null=True, blank=True, on_delete=models.SET_NULL,
                                        related_name="subsetores")
+    ativo = models.BooleanField(default=True)
 
     objects = models.Manager()
 
