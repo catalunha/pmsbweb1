b10f20096ff6592b639bd12f9956bb89abce6a00
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2019-04-06 13:58:16 -0300

    add /api/versao/app endpoint para retornar versão do app mobile, /api/versao returna versão atual da api

--- pmsb_web/api/urls.py
+++ pmsb_web/api/urls.py
@@ -1,12 +1,13 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from api.views import VersaoAPIView
+from api.views import VersaoAPIView, VersaoAppAPIView
 
 app_name = 'api'
 
 urlpatterns = [
     path('versao/', VersaoAPIView.as_view(), name='versao'),
+    path('versao/app/', VersaoAppAPIView.as_view(), name='versao_app'),
     path('auth/', include('api.urls_auth')),
     path('questionarios/', include('questionarios.api_urls')),
     path('docs/', include('api.urls_docs')),
--- pmsb_web/api/views.py
+++ pmsb_web/api/views.py
@@ -2,10 +2,21 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from django.conf import settings
 
 from api.models import MobileApp
 
 class VersaoAPIView(APIView):
+    permission_classes = (AllowAny, )
+
+    def get(self, request, formt=None):
+        """
+        Retorna versão atual da api
+        """
+        
+        return Response(settings.API_VERSION)
+
+class VersaoAppAPIView(APIView):
     permission_classes = (AllowAny, )
 
     def get(self, request, formt=None):
