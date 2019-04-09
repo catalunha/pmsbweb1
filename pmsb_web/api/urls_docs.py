7d7ea16917fe703c0fd6625ffd4197ca8277cfbb
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2019-04-09 00:56:32 -0300

    requere login e is_staff=True para ver documentação da api

--- pmsb_web/api/urls_docs.py
+++ pmsb_web/api/urls_docs.py
@@ -1,6 +1,16 @@
+from django.contrib.auth.decorators import (login_required, user_passes_test)
 from django.urls import path
 from rest_framework_swagger.views import get_swagger_view
 
+
+def user_is_staff(user):
+    return user.is_staff
+
+
+login_required_docs = login_required(get_swagger_view(title="PMSB API"))
+
+docs = user_passes_test(test_func=user_is_staff)(login_required_docs)
+
 urlpatterns = [
-    path('swagger', get_swagger_view(title="PMSB API")),
+    path('swagger', docs),
 ]
